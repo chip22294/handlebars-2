7ce3f27c172af47dbbcc4de0c27dabdf2ab3d716
BigMag <bigmag@MacBook-Pro-BigMag.local>
2023-02-28 18:35:43 +0200

    init

---
+++ routes/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+const hbs = require("hbs");
+
+router.get("/", function (req, res, next) {
+  res.render("index");
+});
+
+router.get("/1", function (req, res, next) {
+  res.render("1-var", { 
+    name: "Dima"
+  });
+});
+
+router.get("/2", function (req, res, next) {
+  res.render("2-with", { 
+    user: {
+      name: "Dima"
+    }, 
+    role: 'Admin'  
+  });
+});
+
+module.exports = router;
