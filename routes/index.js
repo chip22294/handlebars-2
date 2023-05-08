cf049ea43449696df3b921700ebe49fdaa8a4b30
chip22294 <chip22294@gmail.com>
2023-05-08 19:44:21 +0300

    Some changes with prettier

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,20 @@
-const express = require("express");
-const router = express.Router();
-const hbs = require("hbs");
+/* eslint-disable no-unused-vars */
+const express = require("express")
+const router = express.Router()
+const hbs = require("hbs")
 
 router.get("/", function (req, res, next) {
+  const newLocal = 23
   res.render("index", {
     pageTitle: "Shablonizator /",
-    stringVar: 'test',
+    stringVar: "test",
     numberVar: 222,
     boolVar: true,
     listStrVar: ["item1", "item2", "item3"],
     listNumberVar: [0, 1, 2, 3, 4, 5],
     objectVar: {
       objParam1: "value1",
-      objParam2: 23
+      objParam2: newLocal,
     },
     users: [
       {
@@ -20,47 +22,53 @@ router.get("/", function (req, res, next) {
         surname: "Berezovets",
         age: 10,
         isAdmin: true,
-        comments: [
-           {
-             id: 4323,
-             text: "Hello, world !!!"
-           },
-           {
+        _comments: [
+          {
+            id: 4323,
+            text: "Hello, world !!!",
+          },
+          {
             id: 2345,
-             text: "Слава Україні !!!"
-           }
-         ]
+            text: "Слава Україні !!!",
+          },
+        ],
+        get comments() {
+          return this._comments
+        },
+        set comments(value) {
+          this._comments = value
+        },
       },
-       {
-         name: "Mikola",
-         surname: "Shtepa",
-         age: 35,
-         isAdmin: false
-       }
-     ]
-  });
-});
+      {
+        name: "Mikola",
+        surname: "Shtepa",
+        age: 35,
+        isAdmin: false,
+      },
+    ],
+  })
+})
 
+// eslint-disable-next-line no-unused-vars
 router.get("/1", function (req, res, next) {
-  res.render("1-var", { 
+  res.render("1-var", {
     name: "Dima",
     surname: "Brilyukov",
     random: Math.random(),
     user: {
       login: "login",
-      password: "password"
-    }
-  });
-});
+      password: "password",
+    },
+  })
+})
 
 router.get("/2", function (req, res, next) {
-  res.render("2-with", { 
+  res.render("2-with", {
     user: {
-      name: "Dima"
-    }, 
-    role: 'Admin'  
-  });
-});
+      name: "Dima",
+    },
+    role: "Admin",
+  })
+})
 
-
-module.exports = router;
+module.exports = router
